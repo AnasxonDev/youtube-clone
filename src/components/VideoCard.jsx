adde4eb651885a3cf84e7ce2e6276948071f87c6
AnasxonDev <anasxonqwerty@gmail.com>
2024-03-12 16:22:29 +0500

    update

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -3,11 +3,11 @@ import { valueConverter } from "../data";
 
 const VideoCard = ({ thumbnail, title, channelName, views, time }) => {
   return (
-    <div className="font-roboto size-full max-w-[500px]">
+    <div className="font-roboto size-full max-w-[500px] cursor-pointer">
       <img
         src={thumbnail}
         alt="thumbnail"
-        className={`mb-2 w-full transform rounded-xl duration-200 hover:rounded-none`}
+        className="mb-2 w-full transform rounded-xl duration-200 hover:rounded-none"
       />
       <h1 className="max-h-[50px] truncate text-wrap text-[16px] font-semibold">
         {title}
