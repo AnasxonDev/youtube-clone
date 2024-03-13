4455751bf917cc11dba11f51ca1dfe526d798fa0
AnasxonDev <anasxonqwerty@gmail.com>
2024-03-13 21:40:47 +0500

    fix the bug

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -54,7 +54,7 @@ const Videos = ({ category, isOpen, isLoading, setIsLoading, isDarkMode }) => {
   const cards = calculateCardsPerRow();
 
   return (
-    <div className={`${isDarkMode ? "dark" : ""}`}>
+    <div className={`${isDarkMode ? "dark" : ""} overflow-auto`}>
       <div
         className={`grid dark:bg-[#0F0F0F] dark:text-white ${isOpen ? "md:grid-cols-3" : "xl:grid-cols-4"} place-items-center gap-x-4 gap-y-[40px] overflow-auto p-4 sm:grid-cols-2 md:grid-cols-3`}
       >
